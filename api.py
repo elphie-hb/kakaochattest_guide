8fa22f4d41e20d2ad08425503416a1c092559d6f
boyounlim <143076199+boyounlim@users.noreply.github.com>
2023-12-07 00:46:56 +0900

    Update16 api.py test16

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ async def home():
     page = """
     <html>
         <body>
-            <h2>카카오 챗봇빌더 스킬 예제입니다 ^^15</h2>
+            <h2>카카오 챗봇빌더 스킬 예제입니다 ^^16</h2>
         </body>
     </html>
     """
@@ -35,7 +35,6 @@ def skill(req: ChatbotRequest):
     output_text = response.choices[0].message.content
     simple_text_sample2 = {
         "version": "2.0",
-        "useCallback" : true,
         "template": {
             "outputs": [
                 {
